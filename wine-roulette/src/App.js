be54dd2b136306233c9c3764b76f17b7069e802d
Camille <camille.coutens@gmail.com>
2018-10-18 18:39:16 +0200

    Dossier photos vignes pour CARD

--- wine-roulette/src/App.js
+++ wine-roulette/src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-=======
 import Header from "./Header";
-import Carte from "./Carte";
->>>>>>> 620f24f4574211b475aee74feb81e1769f5e8818
-import logo from './logo.svg';
+
+
 import Prix from "./Prix"
 import Appelapi from "./AppelAPI"
 import Colorbutton from "./Colorbutton"
-<<<<<<< HEAD
-=======
+import BoutonPulse from "./BoutonPulse"
 
->>>>>>> 620f24f4574211b475aee74feb81e1769f5e8818
 import './App.css';
 
 class App extends Component {
@@ -19,11 +14,7 @@ class App extends Component {
   // const selectedprice = (prix) => {}
     return (
 		<div className="App">
-<<<<<<< HEAD
 
-=======
-      <Header />
->>>>>>> 620f24f4574211b475aee74feb81e1769f5e8818
 			<img src='http://image.noelshack.com/fichiers/2018/41/5/1539339298-wine.jpg'  alt="logprincipal" className="logoWine" />
 			<h3>Votre bouteille sans prise de tête</h3>
 			
@@ -34,13 +25,9 @@ class App extends Component {
 			<Colorbutton />
 
 			<Appelapi />
-<<<<<<< HEAD
 
       <BoutonPulse />
 
-=======
-      <Carte />
->>>>>>> 620f24f4574211b475aee74feb81e1769f5e8818
 
 
 		</div>
