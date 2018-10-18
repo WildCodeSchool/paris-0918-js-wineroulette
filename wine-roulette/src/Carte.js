3a554efc427fa8c61ddafdf0acb8ef3135f14730
Elisa <elucchini17@gmail.com>
2018-10-18 18:30:45 +0200

    ajout d'un filtre sur l'API pour ne recevoir que les vins dont le package est bouteille + ajustement de la carte (margins)

--- wine-roulette/src/Carte.js
+++ wine-roulette/src/Carte.js
@@ -1,71 +1,104 @@
 import React, { Component } from "react";
-import DisplayCarte from "./DisplayCarte"
+import DisplayCarte from "./DisplayCarte";
 import Vignes from "./imageVignes.jpg";
-import myWines from "./myWineList.json"
-//import Bouteille from "./chat.jpeg" 
+import myWines from "./myWineList.json";
+//import Bouteille from "./chat.jpeg"
 
 class Carte extends Component {
+  state = {
+    data: null
+  };
 
-  state={
-    data: null,
-  }
-
+  //UTILISATION DU FETCH
   // componentDidMount(){
   //   fetch('https://lcboapi.com/products?q=wine&where_not=is_dead&per_page=100&access_key=MDoyOTgwZTZhNi1jNzFjLTExZTgtYTI4Yi0yYjk0MTk2ZTlkMGQ6UkkwcjMzSHF5MW9NMEFxUk9OYXE2V3Y5dFlYRnBiell5THJX')
   //   .then(res => res.json())
   //   .then(obj => this.setState({data:obj.result}))
   //   componentDidMount()}
 
-  componentDidMount(){this.setState({
-    data: myWines
-  })
+  componentDidMount() {
+    this.setState({
+      data: myWines
+    });
   }
 
   render() {
+    //MESSAGE DE CHARGEMENT DE PAGE
+    if (this.state.data === null) return "Wine is coming...";
 
-    if(this.state.data === null)
-      return "Wine is coming..."
+    //GÉNÈRE UN INDEX AU HASARD
+    let randomIndex = Math.floor(Math.random() * Math.floor(100));
 
-    
-    console.log(this.state.data[0])
-    let randomIndex = Math.floor(Math.random() * Math.floor(100))
-    //enlève la partie région si non spécifiée
-    const enleverUndefined = (paysRegion) => {
-      return paysRegion.includes('Region Not Specified')?paysRegion.split(',')[0] : paysRegion
-    }
-
-    //split les mots et les renvoie avec des #
-    const hashtagMyTags = (style) => {
-      return style.split("& ")
-      .map(tag => {
-          return `#${tag}`})
-      .join(" ")
-  }
-
-    let carteVin = {
-      name: this.state.data[randomIndex].name,
-      pays_region: enleverUndefined(this.state.data[randomIndex].origin),
-      annee: this.state.data[randomIndex].released_on.slice(0,4), //ne sélectionne pas mois et jour
-      imageVignes: Vignes,
-      imageBouteille: this.state.data[randomIndex].image_thumb_url,
-      descriptionCourte: `Serving suggestion: ${this.state.data[randomIndex].serving_suggestion}`,
-      tags: `${hashtagMyTags(this.state.data[randomIndex].style)}`,
-      price: `${this.state.data[randomIndex].price_in_cents/100} $`, //transforme prix centimes en prix euro
-      descriptionDetaillee: this.state.data[randomIndex].tasting_note
+    //ENLEVE LA PARTIE "RÉGION" SI NULL
+    const enleverUndefined = paysRegion => {
+      return paysRegion.includes("Region Not Specified")
+        ? paysRegion.split(",")[0]
+        : paysRegion;
     };
+
+    //SPLIT LA PARTIE TAGS ET RENVOIE LES TAGS AVEC DES "#"
+    const hashtagMyTags = style => {
+      return style
+        .split("& ")
+        .map(tag => {
+          return `#${tag}`;
+        })
+        .join(" ");
+    };
+
     
-    //const imageVin = this.state.data[0]
-    //console.log(imageVin)
+    // const tabPackage = this.state.data[0].package.split(" ")[2]
+    // //const tabPackage = this.state.data[0].package.charAt(0)
+    // console.log(tabPackage)
+    // const packageBottle = this.state.data.filter(element => {
+    //   console.log(element.package)
+    //   return element.package !== null && element.package.split(" ")[2] === "bottle"})[randomIndex]
     
+    // FILTER POUR N'AVOIR QUE DES PACKAGES BOUTEILLE
+    const packageBottle = this.state.data.filter(element => {
+      return element.package !== null && element.package.split(" ")[2] === "bottle"
+    })[randomIndex]
+   
+    //FICHE IDENTITÉ DE MA CARTE
+    let carteVin = {
+      name: packageBottle.name,
+      pays_region: enleverUndefined(packageBottle.origin),
+      annee: packageBottle.released_on.slice(0, 4), //ne sélectionne pas mois et jour
+      imageVignes: Vignes,
+      imageBouteille: packageBottle.image_thumb_url,
+      descriptionCourte: `Serving suggestion: ${
+        packageBottle.serving_suggestion
+      }`,
+      tags: `${hashtagMyTags(packageBottle.style)}`,
+      price: `${packageBottle.price_in_cents / 100} $`, //transforme prix centimes en prix euro
+      descriptionDetaillee: packageBottle.tasting_note
+    };
+
+    //CONSOLE LOG DE MON FILTER MAP
+    // const wineName = this.state.data.splice(0, 50)
+    // .filter(element => element.secondary_category.includes("Red Wine"))
+    // .map((element) => (element.name))
+
+    // console.log(wineName)
+
     return (
       <div>
-            {/* {this.state.data.splice(0,10).map(element => (
-                <img src={element.image_thumb_url} alt={element.name} />
-              
-              ))
-            } */}
+        {/* {this.state.data.map(element => element.package)} */}
 
-            <DisplayCarte carteVin={carteVin} />
+
+        {/* AFFICHER TOUS LES STYLES POUR LES VINS BLANC UNIQUEMENT
+        {this.state.data
+          .filter(element => element.secondary_category === "White Wine")
+          .map(element => element.style)
+          .sort()} */}
+
+        {/* AFFICHER TOUTES LES PHOTOS DE 0 A 10 */}
+        {/* {this.state.data.splice(0,10).map(element => (
+      <img src={element.image_thumb_url} alt={element.name} />              
+      ))
+      } */}
+
+        <DisplayCarte carteVin={carteVin} />
       </div>
     );
   }
--- wine-roulette/src/DisplayCarte.js
+++ wine-roulette/src/DisplayCarte.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from "react";
 import "./DisplayCarte.css";
 
@@ -15,13 +14,15 @@ import Collapse from "@material-ui/core/Collapse";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import red from "@material-ui/core/colors/red";
+//import red from "@material-ui/core/colors/red";
 //import yellow from "@material-ui/core/colors/yellow";
 //import deep orange from '@material-ui/core/colors/deep orange';
 //import ShareIcon from "@material-ui/icons/Share";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 //import MoreVertIcon from "@material-ui/icons/MoreVert";
 import Grid from "@material-ui/core/Grid";
+//import Paper from '@material-ui/core/Paper';
+
 
 //CSS de ma carte
 const styles = theme => ({
@@ -34,13 +35,13 @@ const styles = theme => ({
   },
   vignes: {
     height: "0",
-    paddingTop: "56.25%", // 16:9
+    paddingTop: "56.25%" // 16:9
     //border: "2px solid blue"
   },
   bouteille: {
     height: "0",
     paddingTop: "56.25%", // 16:9
-    paddingBottom: "56.25%",
+    paddingBottom: "56.25%"
     //border: "2px solid red"
   },
   actions: {
@@ -58,14 +59,6 @@ const styles = theme => ({
   },
   expandOpen: {
     transform: "rotate(180deg)"
-  },
-  avatar: {
-    width: "150%",
-    //height:'150%',
-    backgroundColor: red[800] //vin rouge
-    //backgroundColor: yellow[200], //vin blanc
-    //backgroundColor: red[800], //rosé
-    //backgroundColor: yellow[400], //champagne
   },
 
   //CSS de mon bouton Acheter
@@ -107,18 +100,24 @@ class DisplayCarte extends Component {
               {/* Gère la taille à l'intérieur de la carte */}
               <Grid container spacing={0}>
                 <Grid className="vignes" item xs={8}>
-                  <CardMedia
-                    className={classes.vignes}
-                    image={this.props.carteVin.imageVignes}
-                    title="Vignobles"
-                  />
+                  <div className="photoVignes">
+                    <CardMedia
+                      className={classes.vignes}
+                      image={this.props.carteVin.imageVignes}
+                      title="Vignobles"
+                    />
+                  </div>
                 </Grid>
                 <Grid className="bouteille" item xs={4}>
-                  <CardMedia
-                    className={classes.bouteille}
-                    image={this.props.carteVin.imageBouteille}
-                    title="Vignobles"
-                  />
+                  <div className="photoBouteille">
+                    
+                      <CardMedia
+                        className={classes.bouteille}
+                        image={this.props.carteVin.imageBouteille}
+                        title="Vignobles"
+                      />
+                    
+                  </div>
                 </Grid>
               </Grid>
 
