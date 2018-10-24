d936f5759801cda35af41cc13cb8607d0e0099c6
Thomas Chaigneau <thomas.a.chaigneau@gmail.com>
2018-10-24 16:11:45 +0200

    add comments

--- wine-roulette/src/StylesofWine-keyWordsForSearchBar.js
+++ wine-roulette/src/StylesofWine-keyWordsForSearchBar.js
@@ -29,6 +29,7 @@
 
 const myWines = require("./myWineList.json")
 
+// Fonction qui déboublonne
 function cleanArray(array) {
     let i, j, len = array.length, out = [], obj = {};
     for (i = 0; i < len; i++) {
@@ -38,13 +39,15 @@ function cleanArray(array) {
     } return out;
   }
 
-
+// la variable stock tous les mots contenant dans myWine.name, ensuite on dédoublonne et on garde que les mot supérieur à 3 lettre
 let KeyWordInWinesName = [];
 myWines.map(items => items.name).map(item => KeyWordInWinesName.push(...item.split(' ')));
 cleanArray(KeyWordInWinesName).filter(item => item.length > 3)
 
 const suggestions = [];
 
+//on place les mots clès dans un objet de la forme { Label : motclé }, objet que l'on met dans un tableau.
+// ce tableau est destiné a etre lu par le composant SearchBar
 for (let i = 0; i < KeyWordInWinesName.length; i++) {
     let ObjWithLabel = new Object;
     ObjWithLabel.label = KeyWordInWinesName[i];
