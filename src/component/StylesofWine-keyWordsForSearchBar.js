7f3944aa07d156e4c576b2fc80f9142928da7bc4
Romain Castillon <castillonromain01@gmail.com>
2018-11-07 09:39:04 +0100

    ok

--- src/component/StylesofWine-keyWordsForSearchBar.js
+++ src/component/StylesofWine-keyWordsForSearchBar.js
@@ -1,34 +1,42 @@
-const myWines = require("../myWineList.json")
+const myWines = require("../myWineList.json");
 
 // Fonction qui déboublonne
-const cleanArray = (array) => {
-    let i, j, len = array.length, out = [], obj = {};
-    for (i = 0; i < len; i++) {
-      obj[array[i]] = 0;
-    } for (j in obj) {
-      out.push(j);
-    } return out;
+const cleanArray = array => {
+  let i,
+    j,
+    len = array.length,
+    out = [],
+    obj = {};
+  for (i = 0; i < len; i++) {
+    obj[array[i]] = 0;
   }
+  for (j in obj) {
+    out.push(j);
+  }
+  return out;
+};
 
-// la variable keyWord stock tous les mots contenus dans myWine.varietal, 
+// la variable keyWord stock tous les mots contenus dans myWine.varietal,
 // ensuite on dédoublonne les mots supérieurs à 6 lettre
 let keyWord = [];
-myWines.map(items => items.varietal).filter(item => item !==null).map(item => keyWord.push(...item.split(' ')));
+myWines
+  .map(items => items.varietal)
+  .filter(item => item !== null)
+  .map(item => keyWord.push(...item.split(" ")));
 const cleanedkeyWord = cleanArray(keyWord.filter(item => item.length > 6));
 
-//on place ces mots  dans un objet de la forme { value: motclé, Label : motclé }, 
+//on place ces mots  dans un objet de la forme { value: motclé, Label : motclé },
 // on met ces objets dans un tableau.
 // ce tableau est destiné a etre lu par le composant SearchBar
 const suggestions = [];
 for (let i = 0; i < cleanedkeyWord.length; i++) {
-    let ObjWithLabel = {};
-    ObjWithLabel.value = cleanedkeyWord[i];
-    ObjWithLabel.label = cleanedkeyWord[i];
-    suggestions.push(ObjWithLabel)
+  let ObjWithLabel = {};
+  ObjWithLabel.value = cleanedkeyWord[i];
+  ObjWithLabel.label = cleanedkeyWord[i];
+  suggestions.push(ObjWithLabel);
 }
 
 export default suggestions;
-
 
 // Red Wine :
 // Full-bodied
@@ -57,5 +65,3 @@ export default suggestions;
 // Light & Fruity
 // Medium-bodied & Flavourful
 // Rich & Complex
-
-
